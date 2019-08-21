ddde51843a31ea8bf909796394acbb3cffe32747
karthirizzu <47343944+karthirizzu@users.noreply.github.com>
2019-08-21 15:13:13 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,12 @@
+def vowelOrConsonant(x): 
+  
+    if (x == 'a' or x == 'e' or
+        x == 'i' or x == 'o' or x == 'u'): 
+        print("Vowel") 
+    else: 
+        print("Consonant") 
+  
+# Driver code 
+vowelOrConsonant('c') 
+vowelOrConsonant('e') 
+      
